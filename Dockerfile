f955fe8d445867650505828fb86b9b23423d514e
root <root@alp.my.domain>
2020-04-30 13:01:07 +0000

    pushing

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ RUN apk add --no-cache git go chromium
 ENV GOROOT /usr/lib/go
 ENV GOPATH /go
 ENV PATH /go/bin:$PATH
-RUN go get -u github.com/chromedp/chromedp github.com/chromedp/cdproto
+RUN go get -u github.com/chromedp/chromedp github.com/chromedp/cdproto github.com/chromedp/chromedp-proxy
 COPY main.go $GOPATH/src/github.com/elhadley2020/traffic_bot/
 RUN cd /go/src/github.com/elhadley2020/traffic_bot/ && go build
 CMD /go/src/github.com/elhadley2020/traffic_bot/traffic_bot
--- main.go
+++ main.go
@@ -1,94 +1,37 @@
-// Command screenshot is a chromedp example demonstrating how to take a
-// screenshot of a specific element and of the entire browser viewport.
+// Command text is a chromedp example demonstrating how to extract text from a
+// specific element.
 package main
 
 import (
 	"context"
-	"io/ioutil"
 	"log"
-	"math"
-	"fmt"
+	"strings"
 
-	"github.com/chromedp/cdproto/emulation"
-	"github.com/chromedp/cdproto/page"
 	"github.com/chromedp/chromedp"
 )
 
 func main() {
-	// create context
-	ctx, cancel := chromedp.NewContext(context.Background())
+	
+	opts := append(chromedp.DefaultExecAllocatorOptions[:],
+		chromedp.ProxyServer(`http://64.225.24.13:3128`),
+	
+	)
+	allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
 	defer cancel()
 
-	// capture screenshot of an element
-	var buf []byte
-	if err := chromedp.Run(ctx, elementScreenshot(`https://www.google.com/`, `#main`, &buf)); err != nil {
+	// create context
+	ctx, cancel := chromedp.NewContext(allocCtx)
+	defer cancel()
+
+	// run task list
+	var res string
+	err := chromedp.Run(ctx,
+		chromedp.Navigate(`https://www.whatismyip.com/`),
+		chromedp.Text(`.list-group-item py-1 font-weight-bold`, &res, chromedp.NodeVisible, chromedp.ByID),
+	)
+	if err != nil {
 		log.Fatal(err)
 	}
-	if err := ioutil.WriteFile("elementScreenshot.png", buf, 0644); err != nil {
-		log.Fatal(err)
-	}
 
-	// capture entire browser viewport, returning png with quality=90
-	if err := chromedp.Run(ctx, fullScreenshot(`https://brank.as/`, 90, &buf)); err != nil {
-		log.Fatal(err)
-	}
-	if err := ioutil.WriteFile("fullScreenshot.png", buf, 0644); err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("All done")
-}
-
-// elementScreenshot takes a screenshot of a specific element.
-func elementScreenshot(urlstr, sel string, res *[]byte) chromedp.Tasks {
-	return chromedp.Tasks{
-		chromedp.Navigate(urlstr),
-		chromedp.WaitVisible(sel, chromedp.ByID),
-		chromedp.Screenshot(sel, res, chromedp.NodeVisible, chromedp.ByID),
-	}
-}
-
-// fullScreenshot takes a screenshot of the entire browser viewport.
-//
-// Liberally copied from puppeteer's source.
-//
-// Note: this will override the viewport emulation settings.
-func fullScreenshot(urlstr string, quality int64, res *[]byte) chromedp.Tasks {
-	return chromedp.Tasks{
-		chromedp.Navigate(urlstr),
-		chromedp.ActionFunc(func(ctx context.Context) error {
-			// get layout metrics
-			_, _, contentSize, err := page.GetLayoutMetrics().Do(ctx)
-			if err != nil {
-				return err
-			}
-
-			width, height := int64(math.Ceil(contentSize.Width)), int64(math.Ceil(contentSize.Height))
-
-			// force viewport emulation
-			err = emulation.SetDeviceMetricsOverride(width, height, 1, false).
-				WithScreenOrientation(&emulation.ScreenOrientation{
-					Type:  emulation.OrientationTypePortraitPrimary,
-					Angle: 0,
-				}).
-				Do(ctx)
-			if err != nil {
-				return err
-			}
-
-			// capture screenshot
-			*res, err = page.CaptureScreenshot().
-				WithQuality(quality).
-				WithClip(&page.Viewport{
-					X:      contentSize.X,
-					Y:      contentSize.Y,
-					Width:  contentSize.Width,
-					Height: contentSize.Height,
-					Scale:  1,
-				}).Do(ctx)
-			if err != nil {
-				return err
-			}
-			return nil
-		}),
-	}
+	log.Println(strings.TrimSpace(res))
 }
